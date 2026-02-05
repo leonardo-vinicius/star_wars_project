75331c4da8dc54ccf969f741c94833bb6ba1f368
Leonardo Vinicius Wanderley Jatobá da Silva <lvwjs@ic.ufal.br>
2026-02-04 23:31:34 -0300

    deploy test

--- app/main.py
+++ app/main.py
@@ -1,13 +1,5 @@
-from fastapi import FastAPI
 from functions_framework import http
-from fastapi.middleware.wsgi import WSGIMiddleware
-
-app = FastAPI()
-
-@app.get("/")
-def health():
-    return {"status": "ok"}
 
 @http
 def starwars_api(request):
-    return WSGIMiddleware(app)(request)
+    return {"status": "ok"}
